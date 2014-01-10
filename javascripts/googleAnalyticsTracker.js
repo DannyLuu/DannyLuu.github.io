98be53c25b3f0720f4b60da4a9407ef63ae85ce2
DeeMoney <danny.luu@thinkwrap.com>
2014-01-10 01:18:56 -0500

    Fixing the commenting style in Google AnalyticsJS

--- javascripts/googleAnalyticsTracker.js
+++ javascripts/googleAnalyticsTracker.js
@@ -1,15 +1,16 @@
-/**
- * Dynamic Content Implementation of Google Analytics Tracker 
- * Tracking ID:	UA-47035308-1
- * 
- * Use a common include or template to paste the code instead of manually adding it to every page.
- * 
- * PHP Implementation:
- * Create a PHP file named "analyticstracking.php" with the code above and include it on each PHP 
- * template page. Then,  add the following line to each template page immediately after the opening 
- *  <body> tag:
- * <?php include_once("analyticstracking.php") ?>
- */
+///
+// Dynamic Content Implementation of Google Analytics Tracker 
+// Tracking ID:	UA-47035308-1
+// 
+// Use a common include or template to paste the code instead of manually adding it to every page.
+// 
+// PHP Implementation:
+// Create a PHP file named "analyticstracking.php" with the code above and include it on each PHP 
+// template page. Then,  add the following line to each template page immediately after the opening 
+//  <body> tag:
+// <?php include_once("analyticstracking.php") ?>
+///
+
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
